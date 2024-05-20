0aa81bdbf6328fbfebbfcfeb5fc75cd37fc5408d
deokgeneral <alsejrrl@naver.com>
2024-05-20 18:46:07 +0900

    info 배경 좀더 밝게 sklii페이지 node.js/react-router 이미지변경

--- src/componet/randing/skill/skill.jsx
+++ src/componet/randing/skill/skill.jsx
@@ -13,11 +13,11 @@ function Skill() {
           <img src='./icon/html.png' alt='html' />
           <img src='./icon/html-css.png' alt='html-css' />
           <img src='./icon/react.png' alt='리액트' className="react-spin" />
-          <img src='./icon/react-router.png' alt='리액트라우터' />
+          <img src='./icon/reactrouter_logo.png' alt='리액트라우터' />
         </div>
         <div className='skill-img-box02'>
           <img src='./icon/js-icon.png' alt='JS' />
-          <img src='./icon/node-js.png' alt='노드제이에스' />
+          <img src='./icon/nodejs_logo.png' alt='노드제이에스' />
           <img src='./icon/axios.png' alt='액시오스' id='axios' /> 
           <img src='./icon/github.png' alt='깃허브' id='github'/> 
         </div>
